f582e85bbcc65f8f45b8cb94acc0588121634ca0
Dipak-hub <dipakchoudhary651@gmail.com>
2022-09-22 18:32:59 +0530

    fixed youtube video tab loop refreshing issue

--- app/Component/Top-Screens/Video.js
+++ app/Component/Top-Screens/Video.js
@@ -1,59 +1,68 @@
-import axios from 'axios'
-import React from 'react'
-import { useEffect } from 'react'
-import { useState } from 'react'
-import { ActivityIndicator, FlatList, StyleSheet, Text, View } from 'react-native'
-import colors from '../../utils/responsive/colors'
-import NewsCard from '../News-Card/News-Card'
+import axios from 'axios';
+import React from 'react';
+import {useEffect} from 'react';
+import {useState} from 'react';
+import {
+  ActivityIndicator,
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import {useSelector} from 'react-redux';
+import colors from '../../utils/responsive/colors';
+import NewsCard from '../News-Card/News-Card';
+import YoutubeComponent from '../Youtube-Component/Youtube-Component';
 
 function Video() {
+  const [page, setPage] = useState(10);
+  const [posts, setPosts] = useState([]);
+  const {all_posts, video_link, is_loading} = useSelector(state => state.posts);
+  const fetchPost = async () => {
+    const res = await axios.get(
+      `https://www.business-northeast.com/wp-json/wp/v2/posts?categories=7051&_embed&per_page=${page}&filter[orderby]=date&order=desc`,
+    );
+    setPosts(res.data);
+  };
 
-  const [page,setPage]=useState(10)
-  const [posts,setPosts]=useState([])
+  const fetchMoreData = () => {
+    setPage(page + 2);
+  };
 
-  const fetchPost=async()=>{
- 
-      const res = await axios.get(`https://www.business-northeast.com/wp-json/wp/v2/posts?categories=7051&_embed&per_page=${page}&filter[orderby]=date&order=desc`)
-      setPosts(res.data)
-  }
-
-  const fetchMoreData=()=>{
-    setPage(page+2)
-   }
-
-   function renderLoader(){
-    return(
+  function renderLoader() {
+    return (
       <View style={styles.ActivityIndicator}>
-      <ActivityIndicator size={'large'} color={colors.primary} />
-    </View>
-    )
-    
+        <ActivityIndicator size={'large'} color={colors.primary} />
+      </View>
+    );
   }
-  useEffect(()=>{
-    fetchPost()
-  },[page])
+
+  function renderYoutube() {
+    return <YoutubeComponent id={video_link} />;
+  }
+  useEffect(() => {
+    fetchPost();
+  }, [page]);
   return (
     <View>
-    <FlatList
-    contentContainerStyle={{flexGrow:1}}
-    keyExtractor={item=>item.id}
-    data={posts}
-    renderItem={({item})=>(
-      <NewsCard news={item} />
-    )}
-    ListFooterComponent={renderLoader}
-    onEndReachedThreshold={0.2}
-    onEndReached={fetchMoreData}
-   
-    />
+      <FlatList
+        ListHeaderComponent={() => renderYoutube()}
+        contentContainerStyle={{flexGrow: 1}}
+        keyExtractor={item => item.id}
+        data={posts}
+        renderItem={({item}) => <NewsCard news={item} />}
+        ListFooterComponent={renderLoader}
+        onEndReachedThreshold={0.2}
+        onEndReached={() => fetchMoreData()}
+      />
     </View>
-  )
+  );
 }
 const styles = StyleSheet.create({
-  ActivityIndicator:{
-    marginVertical:45,
-    alignItems:'center'
-  }
-})
+  ActivityIndicator: {
+    marginVertical: 45,
+    alignItems: 'center',
+  },
+});
 
-export default Video
+export default Video;
--- app/screen/HomeScreen.js
+++ app/screen/HomeScreen.js
@@ -1,43 +1,49 @@
-import { fontSize } from '@mui/system';
-import { useNavigation } from '@react-navigation/native';
+import {fontSize} from '@mui/system';
+import {useNavigation} from '@react-navigation/native';
 import axios from 'axios';
-import { setUseProxies } from 'immer';
-import React, { useEffect } from 'react'
-import { useState } from 'react';
-import { FlatList, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
-import { ActivityIndicator } from 'react-native-paper';
+import {setUseProxies} from 'immer';
+import React, {useEffect} from 'react';
+import {useState} from 'react';
+import {
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import {ScrollView} from 'react-native-gesture-handler';
+import {ActivityIndicator} from 'react-native-paper';
 import Icon from 'react-native-vector-icons/Ionicons';
-import { useDispatch, useSelector } from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import Floatingbutton from '../Component/Floating-button/Floating-button';
 import NewsCard from '../Component/News-Card/News-Card';
 import YoutubeComponent from '../Component/Youtube-Component/Youtube-Component';
-import { getAllPosts, getCategories, getFirstVideo, getVideoLink } from '../store';
+import {
+  getAllPosts,
+  getCategories,
+  getFirstVideo,
+  getVideoLink,
+} from '../store';
 import colors from '../utils/responsive/colors';
 
 function HomeScreen({navigation}) {
-
-
-
-
   const openDrawer = () => {
     navigation.dispatch(DrawerActions.openDrawer());
     // navigation.setOptions({tabBarStyle: {display: 'none'}});
   };
-  const dispatch=useDispatch()
-  const [page,setPage]=useState(1)
-  const [refresh,setRefresh]=useState(false)
-  
+  const dispatch = useDispatch();
+  const [page, setPage] = useState(1);
+  const [refresh, setRefresh] = useState(false);
 
-  const {categories,is_loading}=useSelector((state)=>state.categories)
-  const {all_posts,first_video,video_link}=useSelector((state)=>state.posts)
-  
+  const {categories} = useSelector(state => state.categories);
+  const {all_posts, video_link, is_loading} = useSelector(state => state.posts);
+
+  console.log(is_loading);
+
   // const [allPosts,setAllPosts]=useState([])
   // const [link,setLink]=useState('')
-  // const [isLoadingFinished,setIsLoadingFinished]=useState(false)
 
-
-  
   // const fetchPosts=async()=>{
   //   const res= await axios.get(`https://www.business-northeast.com/wp-json/wp/v2/posts?per_page=20&page=${page}&_embed`)
   //   setAllPosts([...allPosts,...res.data])
@@ -49,106 +55,95 @@ function HomeScreen({navigation}) {
   //   const response = await axios.get(`https://www.business-northeast.com/wp-json/wp/v2/posts?categories=7051&_embed&per_page=1&filter[orderby]=date&order=desc`)
   //  findFirstVideo(response.data[0].content.rendered, word)
   // }
- 
-   const fetchMoreData=()=>{
-   setPage(page+1)
+
+  const fetchMoreData = () => {
+    setPage(page + 1);
+  };
+
+  function renderYoutube() {
+    return <YoutubeComponent id={video_link} />;
   }
 
-function renderYoutube(){
-  return(
-    
-    <YoutubeComponent
-    id={video_link} />
-  )
-}
-
-  function renderLoader(){
-    return(
+  function renderLoader() {
+    return (
       <View style={styles.ActivityIndicator}>
-      <ActivityIndicator size={'large'} color={colors.primary} />
-    </View>
-    )
-    
+        <ActivityIndicator size={'large'} color={colors.primary} />
+      </View>
+    );
   }
-  const handleRefresh=async()=>{
-    setRefresh(true)
-  dispatch(getAllPosts(page))
-   setRefresh(false)
-  }
+  const handleRefresh = async () => {
+    setRefresh(true);
+    dispatch(getAllPosts(page));
+    setRefresh(false);
+  };
 
-//   function findFirstVideo(text, word){
-//     // console.log(text.length)
-//     const myArray = text.split(" ");
+  //   function findFirstVideo(text, word){
+  //     // console.log(text.length)
+  //     const myArray = text.split(" ");
 
-//     var regExp = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/
-//     var match = text.match(regExp);
+  //     var regExp = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/
+  //     var match = text.match(regExp);
 
-//     // console.log(match[2].slice(0,11))
-//     if (match) {
-//       setLink(match[2].slice(0,11))
-//       setIsLoadingFinished(true)
-//     } else {
-//       console.log("Not found")
-//     }
-    
-//     // var x = 0, y=0;
-   
-//     // for (i=0;i< text.length;i++)
-//     //     {
-//     //     if(text[i] == word[0])
-//     //         {
-//     //           console.log(text)
-//     //         for(j=i;j< i+word.length;j++)
-//     //            {
-//     //             if(text[j]==word[j-i])
-//     //               {
-//     //                 y++;
-//     //               }
-//     //             if (y==word.length){
-//     //                 x++;
-//     //             }
-//     //         }
-//     //         y=0;
-//     //     }
-//     // }
-//     // console.log("'"+word+"' was found "+x+" times.");
-// }
+  //     // console.log(match[2].slice(0,11))
+  //     if (match) {
+  //       setLink(match[2].slice(0,11))
+  //       setIsLoadingFinished(true)
+  //     } else {
+  //       console.log("Not found")
+  //     }
 
-  useEffect(()=>{
-    dispatch(getAllPosts(page))
-    dispatch(getCategories())
-    dispatch(getFirstVideo())
-    dispatch(getVideoLink())
+  //     // var x = 0, y=0;
+
+  //     // for (i=0;i< text.length;i++)
+  //     //     {
+  //     //     if(text[i] == word[0])
+  //     //         {
+  //     //           console.log(text)
+  //     //         for(j=i;j< i+word.length;j++)
+  //     //            {
+  //     //             if(text[j]==word[j-i])
+  //     //               {
+  //     //                 y++;
+  //     //               }
+  //     //             if (y==word.length){
+  //     //                 x++;
+  //     //             }
+  //     //         }
+  //     //         y=0;
+  //     //     }
+  //     // }
+  //     // console.log("'"+word+"' was found "+x+" times.");
+  // }
+
+  useEffect(() => {
+    dispatch(getAllPosts(page));
+    dispatch(getCategories());
+    dispatch(getFirstVideo());
+    dispatch(getVideoLink());
     // fetchVideos()
-  },[page])
-
+  }, [page]);
 
   return (
     <>
-<SafeAreaView style={{flex: 1}}>
-    <FlatList
-    key={all_posts.id}
-    contentContainerStyle={{flexGrow:1}}
-    keyExtractor={(item, index) => String(index)}
-    data={all_posts}
-    renderItem={({item})=>(
-      <NewsCard news={item} />
-    )}
-    ListFooterComponent={renderLoader}
-    onEndReachedThreshold={0}
-    onEndReached={fetchMoreData}
-    ListHeaderComponent={renderYoutube}
-    refreshing={refresh}
-    onRefresh={handleRefresh}
-   
-    />
-    </SafeAreaView>
-  
-    <Floatingbutton/>
+      <SafeAreaView style={{flex: 1}}>
+        <FlatList
+          key={all_posts.id}
+          contentContainerStyle={{flexGrow: 1}}
+          keyExtractor={(item, index) => String(index)}
+          data={all_posts}
+          renderItem={({item}) => <NewsCard news={item} />}
+          ListFooterComponent={renderLoader}
+          onEndReachedThreshold={0}
+          onEndReached={() => fetchMoreData()}
+          ListHeaderComponent={() => renderYoutube()}
+          refreshing={refresh}
+          onRefresh={handleRefresh}
+        />
+      </SafeAreaView>
 
-
-     </>
-  )
+      <Floatingbutton />
+    </>
+  );
 }
 const styles = StyleSheet.create({
   actionButtonIcon: {
@@ -156,10 +151,10 @@ const styles = StyleSheet.create({
     height: 22,
     color: 'white',
   },
-  ActivityIndicator:{
-    marginVertical:16,
-    alignItems:'center'
-  }
+  ActivityIndicator: {
+    marginVertical: 16,
+    alignItems: 'center',
+  },
 });
 
-export default HomeScreen
+export default HomeScreen;
